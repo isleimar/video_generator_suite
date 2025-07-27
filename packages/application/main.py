d35753c0aa6c9c59a9f5566ea95ad2238b28840b
Isleimar Oliveira <isleimar@gmail.com>
2025-07-27 17:49:39 -0300

    Refatorando o conteúdo do arquivo main

--- packages/application/main.py
+++ packages/application/main.py
@@ -37,7 +37,7 @@ def run_pipeline(yaml_path: str, output_path: str, verbose: bool):
         logging.error(f"❌ Ocorreu um erro fatal no pipeline.", exc_info=True)
         # exc_info=True adiciona o traceback completo ao log do erro.
 
-if __name__ == "__main__":
+def main():
     parser = argparse.ArgumentParser(description="Gerador de Vídeo a partir de um arquivo YAML.")
     parser.add_argument("yaml_file", help="Caminho para o arquivo de configuração YAML de entrada.")
     parser.add_argument("-o", "--output", default="output.mp4", help="Caminho para o arquivo de vídeo de saída.")
@@ -46,3 +46,6 @@ if __name__ == "__main__":
     
     args = parser.parse_args()
     run_pipeline(args.yaml_file, args.output, args.verbose)
+
+if __name__ == "__main__":
+    main()
